f8bfadecec9bd36da6d2e9094191b6f5693fb3d7
MinKin <1058797743@qq.com>
2023-04-26 22:59:18 +0800

    添加站点运行时间

---
+++ js/site_runtime.js
@@ -0,0 +1,22 @@
+/* 站点运行时间 */
+function runtime() {
+    window.setTimeout("runtime()", 1000);
+    /* 请修改这里的起始时间 */
+    let startTime = new Date('04/16/2023 9:00:00');
+    let endTime = new Date();
+    let usedTime = endTime - startTime;
+    let days = Math.floor(usedTime / (24 * 3600 * 1000));
+    let leavel = usedTime % (24 * 3600 * 1000);
+    let hours = Math.floor(leavel / (3600 * 1000));
+    let leavel2 = leavel % (3600 * 1000);
+    let minutes = Math.floor(leavel2 / (60 * 1000));
+    let leavel3 = leavel2 % (60 * 1000);
+    let seconds = Math.floor(leavel3 / (1000));
+    let runbox = document.getElementById('run-time');
+    runbox.innerHTML = '本站已运行'
+        + '<span style="font-weight: bold; color: #16982b; font-size: 20px;">' + ((days < 10) ? '0' : '') + days + '</span>' + ' 天 '
+        + '<span style="font-weight: bold; color: #16982b; font-size: 20px;">' +((hours < 10) ? '0' : '') + hours + '</span>' + ' 时 '
+        + '<span style="font-weight: bold; color: #16982b; font-size: 20px;">' +((minutes < 10) ? '0' : '') + minutes + '</span>' + ' 分 '
+        + '<span style="font-weight: bold; color: #16982b; font-size: 20px;">' +((seconds < 10) ? '0' : '') + seconds + '</span>' + ' 秒 ';
+}
+runtime();
